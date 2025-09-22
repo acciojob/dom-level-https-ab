30eefbc754fd47f4190056a77eb0097200e1f013
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-09-22 11:23:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const target = document.getElementById("level");
+
+let level = 0;
+lwt current = target;
+
+while(current) {
+	level++;
+	current = current.parentElement;
+}
+
+alert("The level of the element is: " + level);
